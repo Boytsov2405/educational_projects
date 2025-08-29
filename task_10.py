42031b97fdf0b67db27ee18696a94a8346f68e69
= <boytsov2405@yandex.ru>
2025-08-29 21:52:49 +0300

    fix comma

--- task_10.py
+++ task_10.py
@@ -1,8 +1,11 @@
 import re
 
 def count_words(_sentence:str) -> dict:
-    _words = _sentence.split(" ");
-    _words = [_word.lower() for _word in _words if re.fullmatch('[^\\d\\W]+',re.sub("[^\\w]", "", _word)) is not None]
+    _words = [
+        re.sub(r"[^\w]", "", _word).lower()
+        for _word in _sentence.split()
+        if re.sub(r"[^\w]", "", _word).isalpha()
+    ]
     _uniq = set(_words)
     _result = {_word: _words.count(_word) for _word in _uniq}
     return dict(sorted(_result.items(), key = lambda _item:_item[1],reverse = True))
