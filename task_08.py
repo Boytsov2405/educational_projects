c1aa495b80b73073595cdcfb229a08ff846a39ac
= <boytsov2405@yandex.ru>
2025-08-29 22:35:17 +0300

    fix empty input error

--- task_08.py
+++ task_08.py
@@ -2,22 +2,23 @@ import re
 
 def multiply_numbers(_inputs = None) ->int:
     _result = None
-    if(isinstance(_inputs, list)):
-        if(len(_inputs) ==0):
-            return None
-        for _el in _inputs:
-            _res_num = multiply_numbers(_el)
-            if(_res_num is not None):
-                if _result == None: _result = _res_num
-                else: _result*=_res_num
-    else:
-        _inputs = re.findall('-?\\d', str(_inputs).replace(" ",""))
-        
-        if(len(_inputs)==0):
-            return None
-        _result = 1
-        for _el in _inputs:
-            _result*= int(_el)
+    if(_inputs is not None):
+        if(isinstance(_inputs, list)):
+            if(len(_inputs) ==0):
+                return None
+            for _el in _inputs:
+                _res_num = multiply_numbers(_el)
+                if(_res_num is not None):
+                    if _result == None: _result = _res_num
+                    else: _result*=_res_num
+        else:
+            _inputs = re.findall('-?\\d', str(_inputs).replace(" ",""))
+            
+            if(len(_inputs)==0):
+                return None
+            _result = 1
+            for _el in _inputs:
+                _result*= int(_el)
     return _result
 print(multiply_numbers())      
 print(multiply_numbers(["FA-5F",-5,"3-3",[3,2]]))   
