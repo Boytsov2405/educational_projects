c02133744162a1e545ab821bc9998743550a28c3
= <boytsov2405@yandex.ru>
2025-08-29 21:49:57 +0300

    fix output format

--- task_05.py
+++ task_05.py
@@ -1,13 +1,15 @@
 import datetime
 
 def date_in_future(_date):
-    _current_date = datetime.datetime.now()
+    _current_date = datetime.datetime.now().replace(microsecond=0)
     if(not isinstance(_date, int)):
             return _current_date
-    return _current_date + datetime.timedelta(_date)
+    _future_date = _current_date + datetime.timedelta(days=_date)
+    return _future_date.strftime("%d-%m-%Y %H:%M:%S")
     
 print(date_in_future([])) # => текущая дата
 print(date_in_future(5))
 print(date_in_future("ff"))
 print(date_in_future(2.5))
 print(date_in_future(2)) # => текущая дата + 2 дня
+print(isinstance(date_in_future(2), str))
